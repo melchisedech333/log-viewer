7f5c0dade5fc5a443d5dc63bb4722fef58798cbd
melchisedech333 <melchisedech333@gmail.com>
2022-10-12 12:11:25 -0300

    Updates

--- example/main.c
+++ example/main.c
@@ -3,8 +3,19 @@
 
 int main (int argc, char *argv[])
 {
-    say_debug("Iesus Hominum Salvator!");
+    /**
+     * Detailed message example.
+     */
     say_debug_detail("Iesus Hominum Salvator!");
+
+    /**
+     * Example of common usage.
+     */
+    
+    for (int a=0; a<10; a++) {
+        say_debug("Message %d: Iesus Hominum Salvator!", a);
+        sleep(1);
+    }
 
     return 0;
 }
